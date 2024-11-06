8b9c46a39b0ccc107d613a3d0b28ac0f58099db6
abhaybansal5759 <abhaybansal5759@gmail.com>
2024-11-06 15:54:54 +0530

    vite react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <h1>react is good</h1>
+    <Chai/>
+    <p>Testing</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h3>project is ready with vitereact | AB</h3>
+    )
+}
+
+export default Chai
