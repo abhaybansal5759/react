c0dc339d476dd0a491f8f285f2acb0d3524a0431
abhaybansal5759 <abhaybansal5759@gmail.com>
2024-11-07 20:26:44 +0530

    counter

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+let [counter,setCounter] = useState(0)
+
+// let counter = 0
+
+const addValue = () => {
+  console.log("clicked", counter);
+  counter = counter+1  
+  setCounter( counter)
+}
+const removeValue = () => {
+  if(counter>0){
+console.log("remove click",counter);
+counter = counter-1
+setCounter(counter)
+}
+else {
+  console.log("Counter value cannot go below 0");
+}
+}
+
+return (
+    <>
+     <h1>yoo broo</h1>
+     <h2>counter value :{counter}</h2>
+
+     <button
+     onClick={addValue}
+     >Add value</button>
+     <br/>
+     <button onClick={removeValue}>
+      remove value</button>
+    </>
+  )
+}
+
+export default App
