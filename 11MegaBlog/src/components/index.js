7fdeaf2f59311cf1ed3988bce5a49459ba739f10
abhaybansal5759 <abhaybansal5759@gmail.com>
2024-12-28 13:02:37 +0530

    some changes

--- 11MegaBlog/src/components/index.js
+++ 11MegaBlog/src/components/index.js
@@ -3,11 +3,31 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+// import Post from "./Post";
+import PostForm from "./Post-form/PostForm"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from "./Select";
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    // Post,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    Input,
+    Select
 }
 
